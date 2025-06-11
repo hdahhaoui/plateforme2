8f1678e29a6892a6957f2292f3076c964c14d2c5
hdahhaoui <d_hachimi@yahoo.com>
2025-06-11 23:43:55 +0100

    Update database name to freelancing_platform

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $user = 'codiwoob_hassan';
 $pass = 'Sk@teordie2016';
-$db = 'Codify';
+$db = 'freelancing_platform';
 $port = "3306";
 $site = 'Codify';
 
@@ -12,8 +12,8 @@ $favicon = 'asset/favicon.ico';
 
 
 
-$conn = new mysqli($host, $user, $pass, "codiwoob_Codify", $port);
-$mysqli = new mysqli($host, $user, $pass, "codiwoob_Codify", $port);
+$conn = new mysqli($host, $user, $pass, $db, $port);
+$mysqli = new mysqli($host, $user, $pass, $db, $port);
 
 if ($mysqli->connect_errno) {
     echo 'Connect Error ('.$mysqli->connect_errno.') '.$mysqli->connect_error;
